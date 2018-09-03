91938a5a2d9e0711cba59ec57e6cef1d6ea48e29
dfusic <d.fusic@gmail.com>
2018-09-03 14:53:03 +0200

    scss, added onDragend function

--- src/components/Map/index.js
+++ src/components/Map/index.js
@@ -4,15 +4,26 @@ import { Map, TileLayer, Marker } from 'react-leaflet'
 
 export default class MapComp extends Component {
 
-  mapRef = createRef()
+  
+  mapRef = createRef();
+
+  refmarker = React.createRef()
+  
+  updatePosition = () => {
+    const { lat, lng } = this.refmarker.current.leafletElement.getLatLng()
+    console.log({lat, lng});
+    console.log(this.state);
+  }
 
   render() {
     // check if user location is found, if is then allocate the longitude and latitude to marker and place it on the map
     const marker = this.props.foundUserLocation ? (
       <Marker 
       draggable={true}
+      onDragend={this.updatePosition}
       position={[this.props.lat, this.props.lon]}
-      />
+      ref={this.refmarker}
+      ></Marker>
     ) : null;
 
     let mapRender = null;
